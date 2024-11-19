6875fdcb71da12e76a749342c1129ab563d2f7a9
wilrrama <wilrrama@hotmail.com>
2024-11-18 21:50:11 -0300

    feat: project created

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const LivroDigital: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-200">
+      <h1>Livro Digital</h1>
+    </div>
+  );
+};
+
+export default LivroDigital;
