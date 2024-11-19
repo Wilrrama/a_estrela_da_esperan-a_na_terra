deaa33d7e55e374e254ab60b29e005fd374864d3
wilrrama <wilrrama@hotmail.com>
2024-11-18 23:24:03 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+
 const LivroDigital: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-200">
-      <h1>Livro Digital</h1>
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-200">
+        <h1>Livro Digital</h1>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="absolute left-0 top-0 h-16 w-full flex items-center px-4 border-b-2 bg-slate-900 justify-between gap-4 pl">
+      <div className="text-slate-50">Indice</div>
+      {/* <h2 className="text-slate-50  text-2xl text-center">
+        A Estrela da Esperança <br /> na Terra
+      </h2> */}
+      <div className="text-slate-50">Aumentar Fonte</div>
+    </header>
+  );
+};
+
+export default Header;
